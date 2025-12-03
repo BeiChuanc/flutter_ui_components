5f876b7e50328416cfac0fc39f9bd8f21a2a8a39
BeiChuanc <1486129104@qq.com>
2025-12-03 13:33:24 +0800

    v0.0.1

---
+++ custom_components/lib/src/custom_components.dart
@@ -0,0 +1,33 @@
+/*
+ * Created by 周灿 on 2025/12/02.
+ * email: 1486129104@qq.com
+ */
+
+// ignore: unnecessary_library_name
+library custom_components;
+
+/* 核心库 */
+export 'dart:ui'
+    show
+        ImageFilter,
+        BlendMode,
+        Offset,
+        Rect,
+        Color,
+        Paint,
+        Canvas,
+        Size,
+        TextDirection,
+        FontWeight,
+        Locale;
+export 'dart:io';
+export 'dart:math';
+export 'dart:async';
+export 'dart:convert';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+export 'package:flutter/gestures.dart';
+
+/* 三方库 */
+
+/* 组件模块 */
